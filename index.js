9245ccf7b653dcec09fdd602b3e27fbb4b7673ef
Abishek Khanal <45481516+abishek12@users.noreply.github.com>
2024-08-27 13:53:24 +0545

    change in the url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   return res.status(200).json({
     status: 200,
     message: "Server is up",
